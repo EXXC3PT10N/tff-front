60bd8cb1c1d17e187c8051b0b4da7f04fc594497
Mati <zychmateusz11@gmail.com>
2018-03-15 22:34:43 +0100

    home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
-  
+
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: [
+    './assets/css/Footer-Dark.css',
+    './assets/css/Highlight-Blue.css',
+    './assets/css/Highlight-Blue-1.css',
+    './assets/css/Highlight-Clean.css',
+    './assets/css/Highlight-Phone.css',
+    './assets/css/Navigation-Clean.css',
+    './assets/css/styles.css',
+    './assets/fonts/ionicons.min.css',
+    './assets/bootstrap/css/bootstrap.min.css',
+  ]
 })
 export class HomeComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
