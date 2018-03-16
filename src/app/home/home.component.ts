04d9c49ed3e92ce78aef600c113224ad83473c44
Mati <zychmateusz11@gmail.com>
2018-03-16 17:12:36 +0100

    l

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,19 +6,19 @@ import { Component, OnInit } from '@angular/core';
 
   templateUrl: './home.component.html',
   styleUrls: [
-    './assets/css/Footer-Dark.css',
-    './assets/css/Highlight-Blue.css',
-    './assets/css/Highlight-Blue-1.css',
-    './assets/css/Highlight-Clean.css',
-    './assets/css/Highlight-Phone.css',
-    './assets/css/Navigation-Clean.css',
-    './assets/css/styles.css',
-    './assets/fonts/ionicons.min.css',
-    './assets/bootstrap/css/bootstrap.min.css',
+    '../../assets/css/Footer-Dark.css',
+    '../../assets/css/Highlight-Blue.css',
+    '../../assets/css/Highlight-Blue-1.css',
+    '../../assets/css/Highlight-Clean.css',
+    '../../assets/css/Highlight-Phone.css',
+    '../../assets/css/Navigation-Clean.css',
+    '../../assets/css/styles.css',
+    '../../assets/fonts/ionicons.min.css',
+    '../../assets/bootstrap/css/bootstrap.min.css',
   ]
 })
 export class HomeComponent implements OnInit {
-  imagePath: any = 'iphone.svg';
+
   constructor() { }
 
   ngOnInit() {
